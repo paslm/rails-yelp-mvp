2a43b5aa1db617b5bbfe1e69fcac82c00a8876fb
Mirchev Pascal <pascalmirchev2000@gmail.com>
2021-02-18 18:52:07 -0500

    Models are Perfect

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
     validates :content,  presence: true
     belongs_to :restaurant
-    validates :rating, presence: true
+    validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5)} 
 end
- 
+ \
